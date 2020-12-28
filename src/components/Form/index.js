74d95b4a971f20ff68bdab70f7d451918af94003
calpa <calpaliu@gmail.com>
2020-12-28 21:18:33 +0800

    fix: react/jsx-no-target-blank

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,49 +1,49 @@
-import React, { useState } from "react";
-import { Grid, MenuItem, Select, InputLabel } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
-import { DatePicker } from "@material-ui/pickers";
-import { useQuery } from "react-query";
-import dayjs from "dayjs";
-import axios from "axios";
-import currencies from "../../constants/currencies";
-import Result from "../Result";
+import React, { useState } from 'react';
+import { Grid, MenuItem, Select, InputLabel } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { DatePicker } from '@material-ui/pickers';
+import { useQuery } from 'react-query';
+import dayjs from 'dayjs';
+import axios from 'axios';
+import currencies from '../../constants/currencies';
+import Result from '../Result';
 
 const useStyles = makeStyles((theme) => ({
   formControl: {
     margin: theme.spacing(1),
-    minWidth: 120
+    minWidth: 120,
   },
   selectEmpty: {
-    marginTop: theme.spacing(2)
+    marginTop: theme.spacing(2),
   },
   textField: {
-    width: "100%"
-  }
+    width: '100%',
+  },
 }));
 
 export default function Form() {
   const [selectedDate, handleDateChange] = useState(dayjs());
-  const [base, setBase] = useState("");
-  const [toCurrency, setToCurrency] = useState("");
+  const [base, setBase] = useState('');
+  const [toCurrency, setToCurrency] = useState('');
 
   const classes = useStyles();
 
   const { data } = useQuery([base, toCurrency, selectedDate], async () => {
-    const date = selectedDate.format("YYYY-MM-DD");
+    const date = selectedDate.format('YYYY-MM-DD');
     if (date && base && toCurrency) {
       const { data } = await axios.get(
         `https://api.exchangeratesapi.io/${date}?base=${base}&symbols=${toCurrency}`
       );
 
       let rate = data.rates[toCurrency];
-      if (typeof rate === "number") {
+      if (typeof rate === 'number') {
         rate = rate.toFixed(2);
       }
 
       return rate;
     }
 
-    return "Missing the mandatory fields";
+    return 'Missing the mandatory fields';
   });
 
   console.log(data);
@@ -52,16 +52,16 @@ export default function Form() {
     <Grid>
       <Grid container spacing={1}>
         <Grid item xs={12} sm={6}>
-          <Grid container align="flex-start" style={{ margin: 5 }}>
-            <InputLabel id="from-currency">Base Currency</InputLabel>
+          <Grid container align='flex-start' style={{ margin: 5 }}>
+            <InputLabel id='from-currency'>Base Currency</InputLabel>
           </Grid>
 
           <Select
             // placeholder="Currency I Have"
-            labelId="from-currency"
-            defaultValue=""
-            variant="outlined"
-            margin="normal"
+            labelId='from-currency'
+            defaultValue=''
+            variant='outlined'
+            margin='normal'
             className={classes.textField}
             onChange={(e) => setBase(e.target.value)}
           >
@@ -71,14 +71,14 @@ export default function Form() {
           </Select>
         </Grid>
         <Grid item xs={12} sm={6}>
-          <Grid container align="flex-start" style={{ margin: 5 }}>
-            <InputLabel id="from-currency">To Currency</InputLabel>
+          <Grid container align='flex-start' style={{ margin: 5 }}>
+            <InputLabel id='from-currency'>To Currency</InputLabel>
           </Grid>
           <Select
-            label="Currency I Want"
-            defaultValue=""
-            variant="outlined"
-            margin="normal"
+            label='Currency I Want'
+            defaultValue=''
+            variant='outlined'
+            margin='normal'
             className={classes.textField}
             onChange={(e) => setToCurrency(e.target.value)}
           >
@@ -88,25 +88,25 @@ export default function Form() {
           </Select>
         </Grid>
         <Grid item xs={12} sm={6}>
-          <Grid container align="flex-start" style={{ margin: 5 }}>
-            <InputLabel id="from-currency">Date</InputLabel>
+          <Grid container align='flex-start' style={{ margin: 5 }}>
+            <InputLabel id='from-currency'>Date</InputLabel>
           </Grid>
           <DatePicker
             value={selectedDate}
             onChange={handleDateChange}
-            minDate="1999-01-01"
-            labelFunc={(date) => dayjs(date).format("YYYY-MM-DD")}
+            minDate='1999-01-01'
+            labelFunc={(date) => dayjs(date).format('YYYY-MM-DD')}
           />
         </Grid>
 
         <Result data={data} />
 
-        <Grid container item xs={12} justify="center">
+        <Grid container item xs={12} justify='center'>
           Source From&nbsp;
           <a
-            href="https://www.ecb.europa.eu/stats/policy_and_exchange_rates/euro_reference_exchange_rates/html/index.en.html"
-            target="_blank"
-            rel="noreferrer"
+            href='https://www.ecb.europa.eu/stats/policy_and_exchange_rates/euro_reference_exchange_rates/html/index.en.html'
+            target='_blank'
+            rel='noopener noreferrer'
           >
             European Central Bank
           </a>
